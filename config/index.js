c7a58c01ec30a37389ee23151641ab99abe4c38b
diegohp <diego.blake11@gmail.com>
2021-11-07 01:47:46 -0600

    Creacion de usuarios por defecto, películas por defecto y api-key tokens

--- config/index.js
+++ config/index.js
@@ -3,12 +3,24 @@ const config = {
   dev: process.env.NODE_ENV !== 'production',
   port: process.env.PORT || 3000,
 
-  //CONFIG
+  //CONFIG MONGODB
   cors: process.env.CORS,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
+
+  //USERS
+
+  defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
+  defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
+
+  //AUTH
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
+
+  //API KEYS
+  publicApiKeyToken: process.env.PUBLIC_API_KEY_TOKEN,
+  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN,
 };
 
 module.exports = {
